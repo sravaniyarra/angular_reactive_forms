3a28d98afc2b0a9b51e25c725bbeee6c08fb3fbb
sravaniyarra <sravaniyarra55@gmail.com>
2023-11-20 12:07:45 +0530

    Reactive forms

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ReactiveForms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ReactiveForms');
+    // expect(app.title).toEqual('ReactiveForms');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
-export class AppComponent {
-  title = 'ReactiveForms';
+export class AppComponent implements OnInit {
+
+  public profileForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.profileForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^[a-z0-9. _%+-]+@[a-z0-9. -]+\.[a-z]{2,4}$/)]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$/)]],
+      address: ['', Validators.required],
+      pincode: ['', [Validators.required, Validators.pattern(/^[0-9]{6}$/)]]
+    })
+  }
+  onSubmit() {
+
+    if (this.profileForm.valid) {
+      console.log("valid", this.profileForm.value);
+      alert("Registered successfully")
+    }
+    else {
+      console.error("errors")
+    }
+    this.profileForm.reset()
+  }
+
 }
